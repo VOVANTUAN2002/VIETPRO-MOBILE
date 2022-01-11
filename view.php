ba4ce1877eca9f56562ebafca0ef6a2c83fd321c
VOVANTUAN2002 <93719374+VOVANTUAN2002@users.noreply.github.com>
2022-01-11 21:09:07 +0700

    Add files via upload

---
+++ view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Thêm mới khách hàng</h1>
+        <form action="" method="POST" class="form">
+            <div class="form-group">
+                <label>fullname</label>
+                <input value="" name="fullname" class="form-group">
+            </div>
+            <div class="form-group">
+                <label>age</label>
+                <input value="" name="age" class="form-group">
+            </div>
+            <div class="form-group">
+                <label>addres</label>
+                <input value="" name="addres" class="form-group">
+            </div>
+            <button type="text" name="summit">Lưu</button>
+    </div>
+    </form>
+</body>
+
+</html>
